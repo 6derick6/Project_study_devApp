59268ca1903c4c4ac2c064c24efba626be026351
Dérick <6derick6@gmail.com>
2023-03-06 21:20:17 -0300

    Firs publication

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+
+  render(){
+    return (
+      <View>
+        <Text>Minha imagem:</Text>
+        <Image style={{width:50,height:50}}
+        source={{uri: 'https://smartcookinggastronomy.files.wordpress.com/2007/09/frango1.jpg',}}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
