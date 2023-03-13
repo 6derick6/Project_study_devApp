14039e4662c674521357aff426c222dca0289964
Dérick <6derick6@gmail.com>
2023-03-13 12:56:12 -0300

    Lógica de programação com javascript e react 1/4

--- App.js
+++ App.js
@@ -1,46 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
-import Header from './componentes/Header';
-import Body from './componentes/Body';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component{
 
-  constructor(props){
-    super(props);
-    this.mostrarHeader = true;
-  }
+export default function App() {
 
-  mostrarConteudo(){
-    var mostrarBody = 1;
-    if(this.mostrarHeader && mostrarBody == 1){
-      return (
-        <View>
-          <Header />
-          <Body />
-        </View>
-      );
-    }else {
-      return (
-        
-          <Body />
-        
-      );
-    }
+  const idade = 19;
+  var nome = "Dérick";
+  let cidade = "Brochier";
 
-  }
+  const titulo_app = "App de Música";
 
-  render(){
+  return (
+    <View style={{marginTop:Constants.statusBarHeight}}>
 
-    return(
-      <ScrollView style={{marginTop:30,flex:1}}>
-      {
-        this.mostrarConteudo()
-      }
-      </ScrollView>
-    )
-    
-  }
+      <Text>O {nome} mora em {cidade} e tem {idade} anos</Text>
+
+    </View>
+  )
+
 }
 
 
