93f1d92f64078108546c500355ed519b600c7158
Dérick <6derick6@gmail.com>
2023-03-14 22:21:59 -0300

    States

--- App.js
+++ App.js
@@ -1,17 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import Constants from 'expo-constants';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import Body from './components/Body';
 
-export default function App() {
+export default class App extends React.Component {
 
-    return(
-      <View style={{marginTop:30}}>
-        <Body texto="texto 1" titulo="clique aqui - danki code"></Body>
-      </View>
-    );
+  constructor(props){
+    super(props);
 
+    this.state = {
+
+      contador: 0,
+      pagina:'home'
+
+    }
+  }
+
+  alterarAba = (pag) => {
+    this.setState({pagina:pag})
+  }
+
+  render(){
+    if(this.state.pagina == 'home'){
+      return(
+        <View style={{marginTop:30}}>
+          <View><Text>Home</Text></View>
+          <Button onPress={()=>this.alterarAba('contato')} title='CONTATO'></Button>
+        </View>
+      );
+    }else if(this.state.pagina == 'contato'){
+      return(
+        <View style={{marginTop:30}}>
+          <View><Text>Contato</Text></View>
+          <Button onPress={()=>this.alterarAba('home')} title='HOME'></Button>
+        </View>
+      );
+    }    
+  }
 }
 
 
